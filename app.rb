ca75d3b469a8bd3fe49c1cb7cc35b001d422c05e
Kaito-Dogi <kendobu0405@gmail.com>
2021-08-30 05:53:36 +0000

    add: ＋ボタンを実装

--- app.rb
+++ app.rb
@@ -8,3 +8,16 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  @number = Count.find(1).number
+  erb :index
+end
+
+post '/count' do
+  count = Count.find(1)
+  count.number = count.number + 1
+  count.save
+  redirect '/'
+end
+  
